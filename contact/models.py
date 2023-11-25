8abf601a6e65f737094a67dbd91ad8cb8f44007f
ruistaats <rui_staats@hotmail.com>
2023-11-25 20:30:20 -0300

    concluído

--- contact/models.py
+++ contact/models.py
@@ -32,7 +32,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now) 
-    description =  models.TextField()
+    description =  models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y%m')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,19 @@ from contact import views
 
 app_name = 'contact'
 urlpatterns = [
-    path('', views.index, name='index')
+    
+    path('search/', views.search, name="search"),
+    path('', views.index, name='index'),
+    
+    # contact (CRUD)
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
+    path('contact/create/', views.create, name='create'),
+
+    # users
+    path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 ]
